ca26626a2c655fd3c943a1610e25af6facccf0d4
Abhra Dey <abhradey@Abhras-MacBook-Pro.local>
2020-04-06 15:39:19 +0530

    Using go's naked return

---
+++ Practice/SimpleFunction3.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func swapNew(s1, s2 string) (s4, s3 string) {
+	s3 = s1
+	s4 = s2
+	return
+}
+
+func main() {
+	s1 := "Hello"
+	s2 := "World"
+	fmt.Println(s1, s2)
+	s1, s2 = swapNew(s1, s2)
+	fmt.Println(s1, s2)
+}
